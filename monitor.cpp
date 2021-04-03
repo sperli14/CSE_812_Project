4b259452973983715250ce09386231928813576e
sperli14 <46566726+sperli14@users.noreply.github.com>
2021-04-03 14:29:14 -0400

    Added final verdict generation and improved sharing
    -can now share any and all accumulated verdicts with other monitors
    -final verdict generation finished
    -still need to write algorithm to have each monitor actually share its verdict with its neighbors (underlying functionality is finished however)

--- monitor.cpp
+++ monitor.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-using namespace std;
 #include <vector>
+#include <stdio.h>
+using namespace std;
 
 class Monitor {
     public:
@@ -9,26 +10,38 @@ class Monitor {
         //["and","or"]
         vector<bool> observations; //array of different boolean values from monitors
         //[true,false,false]
-
+        //in this example, together this forms "true and false or false"
         int num_monitors = 10; //number of monitors in system
         vector<bool> verdicts; //array of all the verdicts from every monitor, indices are monitor ids
+        vector<bool> verdicts_mod; //array that tells us which verdicts have been modified
 
-        Monitor(int m_id, vector<string> m_subformula, int num_monitors) //constructor
+        Monitor(int m_id, vector<string> m_subformula, int m_num_monitors) //constructor
         {
             id = m_id;
             subformula = m_subformula;
+            num_monitors = m_num_monitors;
             for(int c = 0; c < num_monitors; c++)
+            {
                 verdicts.push_back(false);
+                verdicts_mod.push_back(false);
+            }
+
         }
-        void GetVerdict(Monitor* m, bool verdict)//adopts verdict "verdict" from monitor m, stores it in appropriate index of array
+        void GetVerdict(Monitor* m, unsigned position, bool verdict)//adopts verdict "verdict" from monitor m, stores it in index "position"
         {
-            verdicts[m->id] = verdict;
+            verdicts[position] = verdict;
+            verdicts_mod[position] = true;
+
         }
-        void ShareVerdict(Monitor* m)//shares own verdict with monitor m
+        void ShareVerdict(Monitor* m)//shares own verdicts with monitor m
         {
-            m->GetVerdict(this, true);
+            for(unsigned c=0;c<num_monitors;c++)
+            {
+                if(verdicts_mod[c])
+                    m->GetVerdict(this, c, verdicts[c]);
+            }
         }
-        void Evaluate()
+        void Evaluate()//evaluates own subformula
         {
             vector<bool> sub_verdict;
             for(unsigned c=1;c<observations.size(); c++)
@@ -53,26 +66,51 @@ class Monitor {
                 }
             }
             verdicts[id] = sub_verdict.back();
+            verdicts_mod[id] = true;
+        }
+        bool Evaluate_All()//evaluates all subformulas into one formula
+        {
+            //formula is in form S1 and S2 and S3... such that Sk is the kth subformula
+            //if any of these sub-verdicts are false, our verdict is false
+            for(unsigned c=0;c<num_monitors;c++)
+            {
+                if(verdicts[c] == false)
+                    return false;
+            }
+            return true;
         }
 
 };
 
 int main() {
-  vector<string> form;
-  form.push_back("and");
-  Monitor m(0,form, 3);
+  vector<string> form1;
+  form1.push_back("and");
+  vector<string> form2;
+  form2.push_back("and");
+
+  Monitor m(0,form1, 10);
   m.observations.push_back(true);
-  m.observations.push_back(false);
+  m.observations.push_back(true);
+
+  cout << "Before evaluation" << endl;
   cout << m.verdicts[m.id] << endl;
-  //m.verdicts[m.id] = true;
   m.Evaluate();
+  cout << "After evaluation" << endl;
   cout << m.verdicts[m.id] << endl;
-  /*
-  Monitor n(1,form, 3);
-  cout << "before sharing:" << n.verdicts[0] << endl;
+
+  //Adding some other value to show that multiple values get shared
+  m.verdicts[8] = true;
+  m.verdicts_mod[8] = true;
+
+
+  Monitor n(1,form2, 10);
+  for(unsigned c=0;c<10;c++)
+    cout << "Before sharing:" << n.verdicts[c] << endl;
+  cout << endl << "Sharing..." << endl << endl;
   m.ShareVerdict(&n);
-  cout << "after sharing:" << n.verdicts[0] << endl;
-*/
+  for(unsigned c=0;c<10;c++)
+    cout << "After sharing:" << n.verdicts[c] << endl;
+
   return 0;
 }
 
