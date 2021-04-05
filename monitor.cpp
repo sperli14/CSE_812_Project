ac3a639bb1b5044f912e1644494f72455d1575e2
priyavaddineni <60412887+priyavaddineni@users.noreply.github.com>
2021-04-04 21:14:15 -0400

    Revert "Merge branch 'main' of https://github.com/sperli14/CSE_812_Project into main"
    This reverts commit dc1d37cc8decd179ede892253832fd53e127b062, reversing
    changes made to 45067842479c3e4956e7d46e6fab54f0b15dfbd5.

--- monitor.cpp
+++ monitor.cpp
@@ -14,7 +14,7 @@ class Monitor {
         int num_monitors = 10; //number of monitors in system
         vector<bool> verdicts; //array of all the verdicts from every monitor, indices are monitor ids
         vector<bool> verdicts_mod; //array that tells us which verdicts have been modified
-        vector<Monitor*> network; //all other Monitors
+
         Monitor(int m_id, vector<string> m_subformula, int m_num_monitors) //constructor
         {
             id = m_id;
@@ -27,23 +27,18 @@ class Monitor {
             }
 
         }
-        void AddNetwork(vector<Monitor*> m_network)
-        {
-            network = m_network;
-        }
         void GetVerdict(Monitor* m, unsigned position, bool verdict)//adopts verdict "verdict" from monitor m, stores it in index "position"
         {
             verdicts[position] = verdict;
             verdicts_mod[position] = true;
 
         }
-        void ShareVerdict()//shares own verdicts next monitor in stack
+        void ShareVerdict(Monitor* m)//shares own verdicts with monitor m
         {
             for(unsigned c=0;c<num_monitors;c++)
             {
                 if(verdicts_mod[c])
-                    network[(id+1)%num_monitors]->GetVerdict(this, c, verdicts[c]);
-                    //m->GetVerdict(this, c, verdicts[c]);
+                    m->GetVerdict(this, c, verdicts[c]);
             }
         }
         void Evaluate()//evaluates own subformula
@@ -88,71 +83,33 @@ class Monitor {
 };
 
 int main() {
-
-
   vector<string> form1;
   form1.push_back("and");
-  Monitor m1(0, form1, 3);
-  m1.observations.push_back(true);
-  m1.observations.push_back(true);
-/*
-  cout << "Before evaluation" << endl;
-  cout << m1.verdicts[m1.id] << endl;
-  m1.Evaluate();
-  cout << "After evaluation" << endl;
-  cout << m1.verdicts[m1.id] << endl;
-*/
-  //Adding some other value to show that multiple values get shared
-  //m1.verdicts[8] = true;
-  //m1.verdicts_mod[8] = true;
-
-  //m.ShareVerdict(&m2);
-  //for(unsigned c=0;c<3;c++)
-    //cout << "After sharing:" << m2.verdicts[c] << endl;
   vector<string> form2;
-  form2.push_back("or");
-  Monitor m2(1, form2, 3);
-  m2.observations.push_back(true);
-  m2.observations.push_back(true);
+  form2.push_back("and");
 
-  vector<string> form3;
-  form3.push_back("or");
-  Monitor m3(2, form3, 3);
-  m3.observations.push_back(false);
-  m3.observations.push_back(true);
+  Monitor m(0,form1, 10);
+  m.observations.push_back(true);
+  m.observations.push_back(true);
 
-  vector<Monitor*> monitors;
-  monitors.push_back(&m1);
-  monitors.push_back(&m2);
-  monitors.push_back(&m3);
-  m1.AddNetwork(monitors);
-  m2.AddNetwork(monitors);
-  m3.AddNetwork(monitors);
+  cout << "Before evaluation" << endl;
+  cout << m.verdicts[m.id] << endl;
+  m.Evaluate();
+  cout << "After evaluation" << endl;
+  cout << m.verdicts[m.id] << endl;
 
-  m1.Evaluate();
-  m2.Evaluate();
-  m3.Evaluate();
-
-  cout << "m1 before any sharing occurs" << endl;
-  cout << m1.verdicts[0] << endl;
-  cout << m1.verdicts[1] << endl;
-  cout << m1.verdicts[2] << endl << endl;
+  //Adding some other value to show that multiple values get shared
+  m.verdicts[8] = true;
+  m.verdicts_mod[8] = true;
 
 
-  for(unsigned c = 0; c < 3; c++)
-  {
-      for(unsigned d = 0; d < 3; d++)
-      {
-          monitors[d]->ShareVerdict();
-      }
-  }
-
-  cout << "m1 after all sharing ends" << endl;
-  cout << m1.verdicts[0] << endl;
-  cout << m1.verdicts[1] << endl;
-  cout << m1.verdicts[2] << endl << endl;
-
-  cout << "m1's final verdict:" << m1.Evaluate_All() << endl;
+  Monitor n(1,form2, 10);
+  for(unsigned c=0;c<10;c++)
+    cout << "Before sharing:" << n.verdicts[c] << endl;
+  cout << endl << "Sharing..." << endl << endl;
+  m.ShareVerdict(&n);
+  for(unsigned c=0;c<10;c++)
+    cout << "After sharing:" << n.verdicts[c] << endl;
 
   return 0;
 }
